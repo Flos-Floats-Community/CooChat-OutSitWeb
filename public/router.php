92eea2b7549df3903d8ee853276f4b89e90a16a5
unknown <you@example.com>
2026-02-05 14:55:05 +0800

    Router Changed

--- public/router.php
+++ public/router.php
@@ -1,15 +1,16 @@
 <?php
 
 // 应用入口文件
-namespace think;
 
 try {
     // 加载基础文件
     require __DIR__ . '/../vendor/autoload.php';
     
     // 执行应用
-    $app = new App();
-    $app->run()->send();
+    $app = new app\App();
+    $response = $app->http->run();
+    $response->send();
+    $app->http->end($response);
     
 } catch (\Exception $e) {
     echo $e->getMessage() . PHP_EOL;
